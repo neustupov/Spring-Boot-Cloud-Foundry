bf64c40706ffe2bd243aba65cb770de233426c1c
neustupov <neustupov@yandex.ru>
2019-06-01 19:13:12 +0300

    start project

---
+++ src/main/java/ru/neustupov/SpringBootCloudFoundry/SpringBootCloudFoundryApplication.java
@@ -0,0 +1,13 @@
+package ru.neustupov.SpringBootCloudFoundry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCloudFoundryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCloudFoundryApplication.class, args);
+	}
+
+}
