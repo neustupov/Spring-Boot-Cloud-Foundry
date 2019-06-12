dd2909588b46dcd2e209178f4e332ee256169507
neustupov <neustupov@yandex.ru>
2019-06-12 13:27:18 +0300

    added jdbc templates instead jdbc

--- src/main/java/ru/neustupov/SpringBootCloudFoundry/ApplicationConfiguration.java
+++ src/main/java/ru/neustupov/SpringBootCloudFoundry/ApplicationConfiguration.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
@@ -15,5 +16,10 @@ public class ApplicationConfiguration {
   DataSource dataSource(){
     return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
         .setName("customers").build();
+  }
+
+  @Bean
+  JdbcTemplate jdbcTemplate(DataSource dataSource){
+    return new JdbcTemplate(dataSource);
   }
 }
--- src/main/java/ru/neustupov/SpringBootCloudFoundry/service/CustomerService.java
+++ src/main/java/ru/neustupov/SpringBootCloudFoundry/service/CustomerService.java
@@ -1,41 +1,23 @@
 package ru.neustupov.SpringBootCloudFoundry.service;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import javax.sql.DataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import ru.neustupov.SpringBootCloudFoundry.beans.Customer;
 
 @Component
 public class CustomerService {
 
-  private final DataSource dataSource;
+  private final JdbcTemplate jdbcTemplate;
 
-  public CustomerService(DataSource dataSource) {
-    this.dataSource = dataSource;
+  public CustomerService(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
   }
 
   public Collection<Customer> findAll() {
-    List<Customer> customerList = new ArrayList<>();
-
-    try {
-      try (Connection c = dataSource.getConnection()) {
-        Statement statement = c.createStatement();
-        try (ResultSet rs = statement.executeQuery("SELECT * FROM CUSTOMERS")) {
-          while (rs.next()) {
-            customerList.add(new Customer(rs.getLong("ID"),
-                rs.getString("EMAIL")));
-          }
-        }
-      }
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
-    }
-    return customerList;
+    RowMapper<Customer> rowMapper =
+        (rs, i) -> new Customer(rs.getLong("ID"), rs.getString("EMAIL"));
+    return this.jdbcTemplate.query("SELECT * FROM CUSTOMERS", rowMapper);
   }
 }
