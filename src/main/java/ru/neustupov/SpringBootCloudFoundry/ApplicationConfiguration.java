b9016b4eaa956db6c9cd34c3e36f5f7277f36729
neustupov <neustupov@yandex.ru>
2019-06-12 16:35:32 +0300

    added sql script for init start data

--- src/main/java/ru/neustupov/SpringBootCloudFoundry/ApplicationConfiguration.java
+++ src/main/java/ru/neustupov/SpringBootCloudFoundry/ApplicationConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApplicationConfiguration {
 
   public static void main(String[] args) {
+
     SpringApplication.run(ApplicationConfiguration.class, args);
   }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO customer (id, email) VALUES
+('1', 'A'),
+('2', 'B');
